e8f893dbb6889557e98969f34351637a8e7d17f7
rubens <rasagsilva@gmail.com>
2026-07-26 02:53:07 -0300

    Adicionando schema de mensagem

--- rubens_fast/app.py
+++ rubens_fast/app.py
@@ -1,9 +1,13 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from rubens_fast.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
 
     return {'message': 'Olá Mundo'}
